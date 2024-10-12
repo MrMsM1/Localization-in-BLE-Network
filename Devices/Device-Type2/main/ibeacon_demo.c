a686750044a53e3b7686494563fd44e5a8068368
Mohammad Ali <smohammadali110@yahoo.com>
2024-10-12 18:50:29 +0330

    Device Type 2: Supports WIFI (STA Mode) – BLE (iBeacon Class) – MQTT

---
+++ Devices/Device-Type2/main/ibeacon_demo.c
@@ -0,0 +1,345 @@
+/*
+ * SPDX-FileCopyrightText: 2021 Espressif Systems (Shanghai) CO LTD
+ *
+ * SPDX-License-Identifier: Unlicense OR CC0-1.0
+ */
+
+
+
+/****************************************************************************
+*
+* This file is for iBeacon demo. It supports both iBeacon sender and receiver
+* which is distinguished by macros IBEACON_SENDER and IBEACON_RECEIVER,
+*
+* iBeacon is a trademark of Apple Inc. Before building devices which use iBeacon technology,
+* visit https://developer.apple.com/ibeacon/ to obtain a license.
+*
+****************************************************************************/
+
+#include <stdint.h>
+// #include <string.h>
+// #include <stdbool.h>
+#include <stdio.h>
+#include "nvs_flash.h"
+// #include <unistd.h>
+#include "mqtt_client.h"
+#include "lwip/sockets.h"
+#include "lwip/dns.h"
+#include "lwip/netdb.h"
+#include "esp_wifi.h"
+
+#include "esp_bt.h"
+#include "esp_gap_ble_api.h"
+#include "esp_gattc_api.h"
+#include "esp_gatt_defs.h"
+#include "esp_bt_main.h"
+#include "esp_bt_defs.h"
+#include "esp_ibeacon_api.h"
+#include "esp_log.h"
+#include "freertos/FreeRTOS.h"
+
+static const char* DEMO_TAG = "IBEACON_DEMO";
+static const char *TAG = "MAIN";
+
+char location[100];
+bool send_g = false;
+
+extern esp_ble_ibeacon_vendor_t vendor_config;
+esp_mqtt_client_handle_t client;
+
+static void wifi_event_handler(void *event_handler_arg, esp_event_base_t event_base, int32_t event_id, void *event_data)
+{
+    switch (event_id)
+    {
+    case WIFI_EVENT_STA_START:
+        printf("WiFi connecting ... \n");
+        break;
+    case WIFI_EVENT_STA_CONNECTED:
+        printf("WiFi connected ... \n");
+        break;
+    case WIFI_EVENT_STA_DISCONNECTED:
+        printf("WiFi lost connection ... \n");
+        break;
+    case IP_EVENT_STA_GOT_IP:
+        printf("WiFi got IP ... \n\n");
+        break;
+    default:
+        break;
+    }
+}
+
+void wifi_connection()
+{
+    // 1 - Wi-Fi/LwIP Init Phase
+    esp_netif_init();                    // TCP/IP initiation 					s1.1
+    esp_event_loop_create_default();     // event loop 			                s1.2
+    esp_netif_create_default_wifi_sta(); // WiFi station 	                    s1.3
+    wifi_init_config_t wifi_initiation = WIFI_INIT_CONFIG_DEFAULT();
+    esp_wifi_init(&wifi_initiation); // 					                    s1.4
+    // 2 - Wi-Fi Configuration Phase
+    esp_event_handler_register(WIFI_EVENT, ESP_EVENT_ANY_ID, wifi_event_handler, NULL);
+    esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP, wifi_event_handler, NULL);
+    wifi_config_t wifi_configuration = {
+        .sta = {
+            .ssid = "Hello",
+            .password = "12345678"}};
+    esp_wifi_set_mode(WIFI_MODE_STA);
+    esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_configuration);
+    ESP_LOGI(TAG, "WTF");
+    // 3 - Wi-Fi Start Phase
+    esp_wifi_start();
+    // 4- Wi-Fi Connect Phase
+    esp_wifi_connect();
+}
+
+static esp_err_t mqtt_event_handler_cb(esp_mqtt_event_handle_t event)
+{
+    esp_mqtt_client_handle_t client = event->client;
+    switch (event->event_id)
+    {
+    case MQTT_EVENT_CONNECTED:
+        ESP_LOGI(TAG, "MQTT_EVENT_CONNECTED");
+         int msg_id = esp_mqtt_client_subscribe(client, "location", 0);
+         ESP_LOGI(TAG, "sent subscribe successful, msg_id=%d", msg_id);
+         int msg_id2 = esp_mqtt_client_subscribe(client, "send", 0);
+         ESP_LOGI(TAG, "sent subscribe successful, msg_id=%d", msg_id2);
+        // esp_mqtt_client_publish(client, "test", "Hello! This is Mohammad Ali Sharifi MEhr with 99101773, Enjoying HW3 of SIOT course :))", 0, 1, 0);
+        break;
+    case MQTT_EVENT_DISCONNECTED:
+        ESP_LOGI(TAG, "MQTT_EVENT_DISCONNECTED");
+        break;
+    case MQTT_EVENT_SUBSCRIBED:
+        ESP_LOGI(TAG, "MQTT_EVENT_SUBSCRIBED, msg_id=%d", event->msg_id);
+        break;
+    case MQTT_EVENT_UNSUBSCRIBED:
+        ESP_LOGI(TAG, "MQTT_EVENT_UNSUBSCRIBED, msg_id=%d", event->msg_id); 
+        break;
+    case MQTT_EVENT_PUBLISHED:
+        ESP_LOGI(TAG, "MQTT_EVENT_PUBLISHED, msg_id=%d", event->msg_id);
+        break;
+    case MQTT_EVENT_DATA:
+        ESP_LOGI(TAG, "MQTT_EVENT_DATA");
+        printf("\nTOPIC=%.*s\r\n", event->topic_len, event->topic);
+        printf("DATA=%.*s\r\n", event->data_len, event->data);
+        if (strncmp(event->topic, "location", event->topic_len) == 0)
+        {
+            strncpy(location, event->data, event->data_len);
+            location[event->data_len] = '\0'; // Null-terminate the string
+            ESP_LOGI(TAG, "Location data received: %s", location);
+        } else if (strncmp(event->topic, "send", event->topic_len) == 0)
+        {
+            if (strncmp(event->data, "on", event->data_len) == 0)
+            {   
+                send_g = true;
+                ESP_LOGI(DEMO_TAG, "Send state is ON");
+            }
+            else if (strncmp(event->data, "off", event->data_len) == 0)
+            {   
+                send_g = false;
+                ESP_LOGI(DEMO_TAG, "Send state is OFF");
+            }
+        }
+        break;
+    case MQTT_EVENT_ERROR:
+        ESP_LOGI(TAG, "MQTT_EVENT_ERROR");
+        break;
+    default:
+        ESP_LOGI(TAG, "Other event id:%d", event->event_id);
+        break;
+    }
+    return ESP_OK;
+}
+
+static void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_t event_id, void *event_data)
+{
+    // ESP_LOGI(TAG, "Event dispatched from event loop base=%s, event_id=%d", base, event_id);
+    mqtt_event_handler_cb(event_data);
+}
+
+static void mqtt_app_start(void)
+{
+    esp_mqtt_client_config_t mqtt_cfg = {
+		.broker = {
+			.address.uri="mqtt://192.168.235.221:1883",
+		}
+	
+        // .uri = 
+        // .username = "ESP32_99101773",
+        // .password = "99101773"
+    };
+    client = esp_mqtt_client_init(&mqtt_cfg);
+    esp_mqtt_client_register_event(client, ESP_EVENT_ANY_ID, mqtt_event_handler, client);
+    esp_mqtt_client_start(client);
+}
+
+///Declare static functions
+static void esp_gap_cb(esp_gap_ble_cb_event_t event, esp_ble_gap_cb_param_t *param);
+
+#if (IBEACON_MODE == IBEACON_RECEIVER)
+static esp_ble_scan_params_t ble_scan_params = {
+    .scan_type              = BLE_SCAN_TYPE_ACTIVE,
+    .own_addr_type          = BLE_ADDR_TYPE_PUBLIC,
+    .scan_filter_policy     = BLE_SCAN_FILTER_ALLOW_ALL,
+    .scan_interval          = 0x50,
+    .scan_window            = 0x30,
+    .scan_duplicate         = BLE_SCAN_DUPLICATE_DISABLE
+};
+
+#elif (IBEACON_MODE == IBEACON_SENDER)
+static esp_ble_adv_params_t ble_adv_params = {
+    .adv_int_min        = 1600,//0x20,
+    .adv_int_max        = 1600,//0x20,
+    .adv_type           = ADV_TYPE_NONCONN_IND,
+    .own_addr_type      = BLE_ADDR_TYPE_PUBLIC,
+    .channel_map        = ADV_CHNL_ALL,
+    .adv_filter_policy = ADV_FILTER_ALLOW_SCAN_ANY_CON_ANY,
+};
+#endif
+
+
+static void esp_gap_cb(esp_gap_ble_cb_event_t event, esp_ble_gap_cb_param_t *param)
+{
+    esp_err_t err;
+
+    switch (event) {
+    case ESP_GAP_BLE_ADV_DATA_RAW_SET_COMPLETE_EVT:{
+#if (IBEACON_MODE == IBEACON_SENDER)
+        esp_ble_gap_start_advertising(&ble_adv_params);
+#endif
+        break;
+    }
+    case ESP_GAP_BLE_SCAN_PARAM_SET_COMPLETE_EVT: {
+#if (IBEACON_MODE == IBEACON_RECEIVER)
+        //the unit of the duration is second, 0 means scan permanently
+        uint32_t duration = 0;
+        esp_ble_gap_start_scanning(duration);
+#endif
+        break;
+    }
+    case ESP_GAP_BLE_SCAN_START_COMPLETE_EVT:
+        //scan start complete event to indicate scan start successfully or failed
+        if ((err = param->scan_start_cmpl.status) != ESP_BT_STATUS_SUCCESS) {
+            ESP_LOGE(DEMO_TAG, "Scan start failed: %s", esp_err_to_name(err));
+        }
+        break;
+    case ESP_GAP_BLE_ADV_START_COMPLETE_EVT:
+        //adv start complete event to indicate adv start successfully or failed
+        if ((err = param->adv_start_cmpl.status) != ESP_BT_STATUS_SUCCESS) {
+            ESP_LOGE(DEMO_TAG, "Adv start failed: %s", esp_err_to_name(err));
+        }
+        break;
+    case ESP_GAP_BLE_SCAN_RESULT_EVT: {
+        esp_ble_gap_cb_param_t *scan_result = (esp_ble_gap_cb_param_t *)param;
+        switch (scan_result->scan_rst.search_evt) {
+        case ESP_GAP_SEARCH_INQ_RES_EVT:
+            /* Search for BLE iBeacon Packet */
+            if (esp_ble_is_ibeacon_packet(scan_result->scan_rst.ble_adv, scan_result->scan_rst.adv_data_len)){
+                esp_ble_ibeacon_t *ibeacon_data = (esp_ble_ibeacon_t*)(scan_result->scan_rst.ble_adv);
+                ESP_LOGI(DEMO_TAG, "----------iBeacon Found----------");
+                esp_log_buffer_hex("IBEACON_DEMO: Device address:", scan_result->scan_rst.bda, ESP_BD_ADDR_LEN );
+                esp_log_buffer_hex("IBEACON_DEMO: Proximity UUID:", ibeacon_data->ibeacon_vendor.proximity_uuid, ESP_UUID_LEN_128);
+
+                uint16_t major = ENDIAN_CHANGE_U16(ibeacon_data->ibeacon_vendor.major);
+                uint16_t minor = ENDIAN_CHANGE_U16(ibeacon_data->ibeacon_vendor.minor);
+                ESP_LOGI(DEMO_TAG, "Major: 0x%04x (%d)", major, major);
+                ESP_LOGI(DEMO_TAG, "Minor: 0x%04x (%d)", minor, minor);
+                ESP_LOGI(DEMO_TAG, "Measured power (RSSI at a 1m distance):%d dbm", ibeacon_data->ibeacon_vendor.measured_power);
+                ESP_LOGI(DEMO_TAG, "RSSI of packet:%d dbm", scan_result->scan_rst.rssi);
+                
+                char mac_str[18]; // 17 characters for MAC address plus null terminator
+                sprintf(mac_str, "%02x:%02x:%02x:%02x:%02x:%02x", 
+                    scan_result->scan_rst.bda[0], scan_result->scan_rst.bda[1], scan_result->scan_rst.bda[2], 
+                    scan_result->scan_rst.bda[3], scan_result->scan_rst.bda[4], scan_result->scan_rst.bda[5]);
+
+                ESP_LOGI(DEMO_TAG, "MAC address as string: %s", mac_str);
+                
+                if (strcmp(mac_str, "a0:b7:65:4b:9d:8e") == 0 && send_g) {
+                    // Do something when the target MAC address is detected
+                    char json_message[200]; // Adjust the size according to your needs
+                    snprintf(json_message, sizeof(json_message), "{\"RCV\":\"4\", \"RSSI\": %d, \"Location\": \"%s\"}", scan_result->scan_rst.rssi, location);
+                    esp_mqtt_client_publish(client, "/topic", json_message, 0, 1, 0);
+                    ESP_LOGI(DEMO_TAG, "Target MAC address detected!");
+                    // Perform your desired action here
+                 }
+                
+            }
+            break;
+        default:
+            break;
+        }
+        break;
+    }
+
+    case ESP_GAP_BLE_SCAN_STOP_COMPLETE_EVT:
+        if ((err = param->scan_stop_cmpl.status) != ESP_BT_STATUS_SUCCESS){
+            ESP_LOGE(DEMO_TAG, "Scan stop failed: %s", esp_err_to_name(err));
+        }
+        else {
+            ESP_LOGI(DEMO_TAG, "Stop scan successfully");
+        }
+        break;
+
+    case ESP_GAP_BLE_ADV_STOP_COMPLETE_EVT:
+        if ((err = param->adv_stop_cmpl.status) != ESP_BT_STATUS_SUCCESS){
+            ESP_LOGE(DEMO_TAG, "Adv stop failed: %s", esp_err_to_name(err));
+        }
+        else {
+            ESP_LOGI(DEMO_TAG, "Stop adv successfully");
+        }
+        break;
+
+    default:
+        break;
+    }
+}
+
+
+void ble_ibeacon_appRegister(void)
+{
+    esp_err_t status;
+
+    ESP_LOGI(DEMO_TAG, "register callback");
+
+    //register the scan callback function to the gap module
+    if ((status = esp_ble_gap_register_callback(esp_gap_cb)) != ESP_OK) {
+        ESP_LOGE(DEMO_TAG, "gap register error: %s", esp_err_to_name(status));
+        return;
+    }
+
+}
+
+void ble_ibeacon_init(void)
+{
+    esp_bluedroid_init();
+    esp_bluedroid_enable();
+    ble_ibeacon_appRegister();
+}
+
+void app_main(void)
+{
+    ESP_ERROR_CHECK(nvs_flash_init());
+    ESP_ERROR_CHECK(esp_bt_controller_mem_release(ESP_BT_MODE_CLASSIC_BT));
+    esp_bt_controller_config_t bt_cfg = BT_CONTROLLER_INIT_CONFIG_DEFAULT();
+    esp_bt_controller_init(&bt_cfg);
+    esp_bt_controller_enable(ESP_BT_MODE_BLE);
+    
+     
+    ble_ibeacon_init();
+
+    /* set scan parameters */
+#if (IBEACON_MODE == IBEACON_RECEIVER)
+    esp_ble_gap_set_scan_params(&ble_scan_params);
+    wifi_connection();
+    vTaskDelay(2000 / portTICK_PERIOD_MS);
+    mqtt_app_start();
+#elif (IBEACON_MODE == IBEACON_SENDER)
+    esp_ble_ibeacon_t ibeacon_adv_data;
+    esp_err_t status = esp_ble_config_ibeacon_data (&vendor_config, &ibeacon_adv_data);
+    if (status == ESP_OK){
+        esp_ble_gap_config_adv_data_raw((uint8_t*)&ibeacon_adv_data, sizeof(ibeacon_adv_data));
+    }
+    else {
+        ESP_LOGE(DEMO_TAG, "Config iBeacon data failed: %s\n", esp_err_to_name(status));
+    }
+#endif
+}
